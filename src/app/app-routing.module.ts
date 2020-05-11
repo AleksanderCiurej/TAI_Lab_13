fdd016741e4cfdbaf80a30fa02ea7043b7bff77b
Aleksander <aleksander.ciurej@gmail.com>
2020-05-11 20:56:19 +0200

    lab9 finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {ContactComponent} from './components/contact/contact.component';
 import {BlogHomeComponent} from "./components/blog-home/blog-home.component";
 import {BlogItemDetailsComponent} from "./components/blog-item-details/blog-item-details.component";
-
+import {AddPostComponent} from "./components/add-post/add-post.component";
 
 
 const routes: Routes = [
@@ -23,6 +23,10 @@ const routes: Routes = [
   {
     path: 'blog/detail/:id',
     component: BlogItemDetailsComponent
+  },
+  {
+    path: 'add-post',
+    component: AddPostComponent
   }
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { BlogComponent } from './components/blog/blog.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { BlogItemComponent } from './components/blog-item/blog-item.component';
-import { BlogItemTextComponent } from './components/blog-item-text/blog-item-text.component';
-import { BlogItemImageComponent } from './components/blog-item-image/blog-item-image.component';
-import { BlogItemDetailsComponent } from './components/blog-item-details/blog-item-details.component';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {BlogComponent} from './components/blog/blog.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {BlogItemComponent} from './components/blog-item/blog-item.component';
+import {BlogItemTextComponent} from './components/blog-item-text/blog-item-text.component';
+import {BlogItemImageComponent} from './components/blog-item-image/blog-item-image.component';
+import {BlogItemDetailsComponent} from './components/blog-item-details/blog-item-details.component';
 import {DataService} from "./services/data.service";
-import { SummaryPipe } from './pipes/summary.pipe';
-import { SearchBarComponent } from './shared/search-bar/search-bar.component';
-import { BlogHomeComponent } from './components/blog-home/blog-home.component';
-import { FilterPipe } from './pipes/filter.pipe';
-import { TextFormatDirective } from './directives/text-format.directive';
-import { SelectizeComponent } from './components/selectize/selectize.component';
+import {SummaryPipe} from './pipes/summary.pipe';
+import {SearchBarComponent} from './shared/search-bar/search-bar.component';
+import {BlogHomeComponent} from './components/blog-home/blog-home.component';
+import {FilterPipe} from './pipes/filter.pipe';
+import {TextFormatDirective} from './directives/text-format.directive';
+import {SelectizeComponent} from './components/selectize/selectize.component';
+import {AddPostComponent} from './components/add-post/add-post.component';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { SelectizeComponent } from './components/selectize/selectize.component';
     BlogHomeComponent,
     FilterPipe,
     TextFormatDirective,
-    SelectizeComponent
+    SelectizeComponent,
+    AddPostComponent
   ],
   imports: [
     BrowserModule,
